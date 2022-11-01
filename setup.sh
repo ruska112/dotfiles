e9119cfb22e7430498a6add2338a4879de9ae367
Ruslan Karabalin <94182636+ruska112@users.noreply.github.com>
2022-11-01 18:04:45 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-sudo pacman -Syu git htop ranger neovim vlc ffmpeg intellij-idea-community-edition -y
-sudo pacman -S ttf-jetbrains-mono ttf-nerd-fonts-symbols-2048-em-mono noto-fonts-emoji otf-latin-modern otf-latinmodern-math -y
-sudo pacman -S jdk-openjdk maven nodejs npm python python-pip rustup go gcc cmake -y
+sudo pacman -Syu git htop ranger neovim vlc ffmpeg intellij-idea-community-edition ttf-jetbrains-mono ttf-nerd-fonts-symbols-2048-em-mono noto-fonts-emoji otf-latin-modern otf-latinmodern-math jdk-openjdk maven nodejs npm python python-pip rustup go gcc cmake -y
 
 git config --global user.email "karabalin112@gmail.com" && git config --global user.name "Ruslan Karabalin"
 
@@ -19,6 +17,6 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 
 yay -S google-chrome telegram-desktop discord topgrade -y
 
-sudo pacman -S mesa mesa-utils lib32-mesa xf86-video-ati mesa-vdpau lib32-mesa-vdpau vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings xorg-server-devel opencl-nvidia lib32-opencl-nvidia libxnvctrl
+sudo pacman -S mesa mesa-utils lib32-mesa xf86-video-ati mesa-vdpau lib32-mesa-vdpau vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader nvidia nvidia-utils lib32-nvidia-utils nvidia-settings xorg-server-devel opencl-nvidia lib32-opencl-nvidia libxnvctrl
 
 sudo mkinitcpio -P
