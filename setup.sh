3c9c132216784a2845f435e820f0fa2d67a3edb7
Ruslan Karabalin <94182636+ruska112@users.noreply.github.com>
2022-11-06 16:05:53 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -Syu git htop ranger neovim vlc ffmpeg intellij-idea-community-edition ttf-jetbrains-mono ttf-nerd-fonts-symbols-2048-em-mono noto-fonts-emoji otf-latin-modern otf-latinmodern-math jdk-openjdk maven nodejs npm python python-pip rustup go gcc cmake -y
+sudo pacman -Syu git htop tree ranger neovim vlc ffmpeg intellij-idea-community-edition ttf-jetbrains-mono ttf-nerd-fonts-symbols-2048-em-mono noto-fonts-emoji otf-latin-modern otf-latinmodern-math jdk-openjdk maven nodejs npm python python-pip rustup go gcc cmake -y
 
 git config --global user.email "karabalin112@gmail.com" && git config --global user.name "Ruslan Karabalin"
 
