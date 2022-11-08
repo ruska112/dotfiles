dde14a72ac57ca3e1b500863c14d95fe0e056816
Ruslan Karabalin <94182636+ruska112@users.noreply.github.com>
2022-11-08 11:30:42 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,17 +6,14 @@ git config --global user.email "karabalin112@gmail.com" && git config --global u
 
 rustup install stable
 
-mkdir Source && cd Source
+mkdir Sources && cd Sources
 
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+yay -S google-chrome telegram-desktop discord topgrade
 
-yay -S google-chrome telegram-desktop discord topgrade -y
-
-sudo pacman -S mesa mesa-utils lib32-mesa xf86-video-ati mesa-vdpau lib32-mesa-vdpau vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader nvidia nvidia-utils lib32-nvidia-utils nvidia-settings xorg-server-devel opencl-nvidia lib32-opencl-nvidia libxnvctrl
+sudo pacman -S nvidia nvidia-utils lib32-nvidia-utils nvidia-settings xorg-server-devel opencl-nvidia lib32-opencl-nvidia libxnvctrl
 
 sudo mkinitcpio -P
